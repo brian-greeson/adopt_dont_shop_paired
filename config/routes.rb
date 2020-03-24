3b4c05d337a11893dfbf39707feb3d547b74df1f
Steven Meyers <sjjmeyers@gmail.com>
2020-03-24 15:45:30 -0600

    Add root route fixes #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'shelters#index'
 
+  root 'shelters#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
