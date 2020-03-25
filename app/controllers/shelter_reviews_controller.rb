5d269f584f4673c848ed6b1acf5d43b22c041f24
Steven Meyers <sjjmeyers@gmail.com>
2020-03-25 17:52:03 -0600

    Add testing and flash message for shelter review sad paths

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -4,14 +4,22 @@ class ShelterReviewsController < ApplicationController
   end
 
   def create
-    ShelterReview.create!({
+    @shelter = Shelter.find(params[:id])
+
+    shelter_review = ShelterReview.new({
       title: shelter_review_params[:title],
       image: shelter_review_params[:image],
       content: shelter_review_params[:content],
       rating: shelter_review_params[:rating],
       shelter_id: params[:id]
       })
-    redirect_to "/shelters/#{params[:id]}"
+
+    if shelter_review.save
+      redirect_to "/shelters/#{params[:id]}"
+    else
+      flash.now[:ding_dong] = "Please include a title and content for your review."
+      render :new
+    end
   end
 
   private
