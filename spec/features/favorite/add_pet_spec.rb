916746dec0b5a1fb1b86e4b57e5a55f00ccfef48
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-26 14:49:52 -0600

    Add test for favorites to increments

--- spec/features/favorite/add_pet_spec.rb
+++ spec/features/favorite/add_pet_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe "A user I visit any page" do
-  it "I can see a count of my favorite pets" do
+RSpec.describe "A user I visit " do
+  it "any page I can see a count of my favorite pets" do
     shelter_1 = Shelter.create(name: "Denver Animal Shelter", address: "1241 W Bayaud Ave", city: "Denver", state: "CO", zip: "80223")
 
     pet_1 = shelter_1.pets.create(
@@ -16,9 +16,23 @@ RSpec.describe "A user I visit any page" do
       expect(page). to have_content("Favorites: 0")
       visit "/pets"
       expect(page). to have_content("Favorites: 0")
-
   end
+  it "Pets show page I can click a link to favorite that pet" do
+    shelter_1 = Shelter.create(name: "Denver Animal Shelter", address: "1241 W Bayaud Ave", city: "Denver", state: "CO", zip: "80223")
 
+    pet_1 = shelter_1.pets.create(
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Jack_Russell_Terrier_1.jpg",
+      name: "Spot",
+      approximate_age: "5",
+      sex: "male")
 
+      visit pets_show_path(pet_1)
+      click_link "Add Favorite"
 
+      expect(current_path).to eq(pets_show_path(pet_1))
+      within("p.flash-message") do
+        expect(page).to have_content("#{pet_1.name} has been added to your favorites list ❤️ ")
+      end
+      expect(page).to have_content("Favorites: 1")
+  end
 end
