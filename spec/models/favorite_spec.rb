47d6c784bcec3da28f005136638c3aa92f88e06a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-26 17:49:51 -0600

    Changed session contents to strings from ints

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -2,10 +2,23 @@ require "rails_helper"
 
 RSpec.describe Favorite do
   it "can calculate total favorited pets" do
-    favorite = Favorite.new([1,2,3])
+    favorite = Favorite.new(["1","2","3"])
 
     expect(favorite.total_count).to eq(3)
-
   end
+
+  it "can list it's contents" do
+    favorite = Favorite.new(["1","2","3"])
+
+    expect(favorite.contents).to eq(["1","2","3"])
+  end
+
+  it "can add a pet to its contents" do
+    favorite = Favorite.new(["1","2","3"])
+    favorite.add_pet(4)
+
+    expect(favorite.contents).to eq(["1","2","3","4"])
+  end
+
 
 end
