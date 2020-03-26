ff3fe883b7d4f5c593ccf77db3f61f22b926c58a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-26 17:50:51 -0600

    Added favorite controller update method

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,12 +1,12 @@
 class FavoriteController < ApplicationController
   def update
     pet = Pet.find(params[:pet_id])
-    pet_id_string = pet.id.to_s
-    session[:favorite] ||= Array.new
-    session[:favorite] << pet_id_string
+    favorite.add_pet(pet.id)
+    session[:favorite] = favorite.contents
     flash[:ding_dong] = "#{pet.name} has been added to your favorites list ❤️"
     redirect_to "/pets/#{params[:pet_id]}"
   end
 
 end
 # flash[:notice] = "You now have #{session[:favorite][song_id_str]} copy of #{song.title} in your cart."
+# pet_id_string = pet.id.to_s
