401480fd4a613e35b2d8ecf711c736690ae88be8
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-26 17:50:24 -0600

    Created favorites model and total count method

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,11 @@ class Favorite
   attr_reader :contents
 
   def initialize(initial_contents)
-    @contents = initial_contents
+    @contents = initial_contents || Array.new
+  end
+
+  def add_pet(pet_id)
+    @contents << pet_id.to_s
   end
 
   def total_count
