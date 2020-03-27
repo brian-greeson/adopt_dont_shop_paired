65fe53c6659e0b785679e9b9a395365b8928150d
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-26 21:03:56 -0600

    Add edge case testing to favorites index test Closes #11

--- spec/features/favorite/index_spec.rb
+++ spec/features/favorite/index_spec.rb
@@ -14,13 +14,30 @@ RSpec.describe "As a vistor When I have added a pet to my favorite list" do
       approximate_age: "5",
       sex: "male"
     )
-
+    pet_2 = shelter_1.pets.create(
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Jack_Russell_Terrier_2.jpg",
+      name: "Spot 2",
+      approximate_age: "2",
+      sex: "male"
+    )
+    pet_3 = shelter_1.pets.create(
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Jack_Russell_Terrier_3.jpg",
+      name: "Spot 3",
+      approximate_age: "4",
+      sex: "male"
+    )
     visit pets_show_path(pet_1)
+    click_link "Add Favorite"
+    visit pets_show_path(pet_2)
     click_link "Add Favorite"
     visit ("/favorites")
 
     expect(page).to have_link(pet_1.name)
     expect(page).to have_css("img[src*= '#{pet_1.image}']")
+    expect(page).to have_link(pet_2.name)
+    expect(page).to have_css("img[src*= '#{pet_2.image}']")
+    expect(page).to_not have_link(pet_3.name)
+    expect(page).to_not have_css("img[src*= '#{pet_3.image}']")
 
   end
 
