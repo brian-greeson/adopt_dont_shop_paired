238537bef43442cf89fe5e43e4692ca0e1ba73c0
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-28 10:23:43 -0600

    Add link_text_and_method model behavior

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -20,5 +20,12 @@ RSpec.describe Favorite do
     expect(favorite.contents).to eq(["1","2","3","4"])
   end
 
+  it ".link_text_and_method" do
+    favorite = Favorite.new(["1","2","3"])
+    add_favorite_link = {:favorite_link_text=>"Add Favorite", :method=>:patch}
+    remove_favorite_link = {:favorite_link_text=>"Remove Favorite", :method=>:delete}
 
+    expect(favorite.link_text_and_method("1")).to eq(remove_favorite_link)
+    expect(favorite.link_text_and_method("4")).to eq(add_favorite_link)
+  end
 end
