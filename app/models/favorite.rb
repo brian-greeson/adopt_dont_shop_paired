0b3f5e1fda1d193d8437236bc40d1b810f5866af
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-28 10:25:06 -0600

    Created link_text_and_method behavior

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -13,4 +13,16 @@ class Favorite
     @contents.count
   end
 
+  def link_text_and_method(pet_id)
+    favorite_link = {}
+    if contents.include?(pet_id.to_s)
+      favorite_link[:favorite_link_text] = "Remove Favorite"
+      favorite_link[:method] = :delete
+    else
+      favorite_link[:favorite_link_text] = "Add Favorite"
+      favorite_link[:method] = :patch
+    end
+    favorite_link
+  end
+
 end
