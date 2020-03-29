8313c6a9c56b3115d1937c788960e2ff32bd77a3
Steven Meyers <sjjmeyers@gmail.com>
2020-03-29 17:08:02 -0600

    Add for_pet class method for PetApplication model

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,7 +2,7 @@ class PetApplication < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :phone_number, :description, :pet_ids
   has_many :application_pets
   has_many :pets, through: :application_pets
-  
+
   def self.pets
     pets_with_applicatons = []
     all.each do |application|
@@ -10,4 +10,16 @@ class PetApplication < ApplicationRecord
     end
     pets_with_applicatons.flatten.uniq
   end
+
+  def self.for_pet(pet_id)
+    applications_for_pet = []
+    pet_id_integer = pet_id.to_i
+    find_each do |application|
+      if application.pet_ids.include?(pet_id_integer)
+        applications_for_pet << application
+      end
+    end
+    applications_for_pet
+  end
+
 end
