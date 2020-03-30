723f3e5748890705b2f31897044d4e337171b6e4
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-29 21:00:28 -0600

    Add test that applications can be approved and displayed

--- spec/features/pet_applications/show_spec.rb
+++ spec/features/pet_applications/show_spec.rb
@@ -43,4 +43,41 @@ RSpec.describe "As a visitor when I visit the application show page" do
       expect(page).to have_link(pet_1.name)
     end
   end
+
+  it "I can approve the application for each pet on the application" do
+    shelter_1 = Shelter.create(
+      name: "Denver Animal Shelter",
+      address: "1241 W Bayaud Ave",
+      city: "Denver",
+      state: "CO", zip: "80223"
+    )
+
+    pet_1 = shelter_1.pets.create(
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Jack_Russell_Terrier_1.jpg",
+      name: "Spot",
+      approximate_age: "5",
+      sex: "male"
+    )
+
+    app_1 = PetApplication.create(
+      name: 'Steve',
+      address: '123 Main St',
+      city: 'Lakewood',
+      state: 'CO',
+      zip: '80214',
+      phone_number: '9705675555',
+      description: 'I like dogs and will take great care of it.',
+      pet_ids: [pet_1.id]
+    )
+
+    visit "/pet_applications/#{app_1.id}"
+
+    within "section.application-#{app_1.id}-pets" do
+      find("#approve-#{pet_1.id}").click
+    end
+
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+    expect(page).to have_content("pending")
+    expect(page).to have_content("On hold for #{app_1.name}")
+  end
 end
