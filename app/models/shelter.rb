eb8ba3243450f6e73a14755e2073a0ef5bb13220
Steven Meyers <sjjmeyers@gmail.com>
2020-03-31 15:56:27 -0600

    Push in progress for development

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,10 @@ class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets, dependent: :destroy
   has_many :shelter_reviews, dependent: :destroy
+
+  def has_pending_pet?
+    binding.pry
+    pets.include?(approved_application)
+
+  end
 end
