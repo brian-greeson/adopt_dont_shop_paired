334311031d29e26e6682760d3ead6f1432609667
Steven Meyers <sjjmeyers@gmail.com>
2020-03-31 15:05:59 -0600

    Delete Shelter delete spec and move to test to show spec

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -13,6 +13,20 @@ RSpec.describe "shelter page", type: :feature do
     expect(page).to have_content("Zip: #{shelter_1.zip}")
   end
 
+  it 'I can delete a shelter' do
+    shelter_1 = Shelter.create(name: "Denver Animal Shelter", address: "1241 W Bayaud Ave", city: "Denver", state: "CO", zip: "80223")
+    shelter_2 = Shelter.create(name: "Aurora Animal Shelter", address: "15750 E 32nd Ave", city: "Aurora", state: "CO", zip: "80011")
+
+    visit "/shelters/#{shelter_1.id}"
+
+    click_link 'Delete'
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to_not have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
+  end
+
+
   it "there is a navigation link to shelter index and pet index" do
     shelter_1 = Shelter.create(name: "Denver Animal Shelter", address: "1241 W Bayaud Ave", city: "Denver", state: "CO", zip: "80223")
     visit "/shelters/#{shelter_1.id}"
