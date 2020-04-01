92bdfb8f0bdadc71b0d488d2ffa4294ef59e284d
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-31 18:33:24 -0600

    Add incomplete_form flash message on shelter create action

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -15,8 +15,14 @@ class SheltersController < ApplicationController
   end
 
   def create
-    Shelter.create(shelter_params)
-    redirect_to '/shelters'
+    shelter = Shelter.new(shelter_params)
+
+    if shelter.save
+      redirect_to '/shelters'
+    else
+      flash.now[:incomplete_form] = shelter.errors.messages.keys
+      render :new
+    end
   end
 
   def edit
